b5aa51feac9eaf24ec71f4038ca6a3951052a607
amaar1082 <amaarhaider108@gmail.com>
2026-02-21 17:08:36 +0500

    Creat bussiness and car for PF week 2 lab.
    Creat bussiness and car for PF week 2 lab.

---
+++ bussiness/bussiness.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+main()
+{
+cout<<"====================================================================="<<endl;
+cout<<"                         POINT OF SALE SYSTEM                        "<<endl;
+cout<<"====================================================================="<<endl;
+cout<<"=====================------------MANU----------======================  "<<endl<<endl;
+cout<<"1 ADD NEW PRODUCT"<<endl;
+cout<<"2 VIEW ALL PRODUCT "<<endl;
+cout<<"3 MAKE SALES"<<endl;
+cout<<"4 VIEW SALES REPORT "<<endl;
+cout<<"5 SEARCH PRODUCT"<<endl;
+cout<<"6 DELETE PRODUCT"<<endl;
+cout<<"7 EXIT"<<endl<<endl;
+cout<<"======================================================================"<<endl;
+cout<<"Please select an option (1-7)"<<endl;
+}
